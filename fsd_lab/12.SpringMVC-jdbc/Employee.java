66ef05c3c81d09374f688c9378ca29ee0391529a
Deekshitha Ganji <163876391+deekshitha-ganji@users.noreply.github.com>
2024-12-14 10:50:21 +0530

    Create Employee.java

---
+++ fsd_lab/12.SpringMVC-jdbc/Employee.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.beans;
+
+/**
+ *
+ * @author Deekshitha
+ */
+public class Employee {
+    private int empId;
+    private String name;
+    private int deptNo;
+    private String branch;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDeptNo() {
+        return deptNo;
+    }
+
+    public void setDeptNo(int deptNo) {
+        this.deptNo = deptNo;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+    
+}
